f7073f9295326d9957b93c25a342306011e7105c
平凡 <2069074269@qq.com>
2022-09-28 17:03:13 +0800

    第一次提交！

--- src/main/java/nxu/it/controller/MainController.java
+++ src/main/java/nxu/it/controller/MainController.java
@@ -4,10 +4,17 @@ import com.jfinal.core.Controller;
 import com.jfinal.core.Path;
 
 
-@Path(value = "/", viewPath = "/")
+@Path(value = "/")
 public class MainController extends Controller {
     public void index() {
         renderHtml("<h2>It works!</h2>");
     }
 
+    public void hello(){
+        String username=get("username");
+        String htmlTpl="<h2>你好!</h2>";
+        String html=String.format(htmlTpl,username);
+        renderHtml(html);
+    }
+    public void bye(){renderHtml("<h2>再见！</h2>");}
 }
